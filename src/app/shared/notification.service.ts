bdc92f374a264f60f4ad461df8afa9cd050d4ffc
Randy rebucas <rebucasrandy1986@gmail.com>
2019-08-26 15:14:37 +0800

    update notification

--- src/app/shared/notification.service.ts
+++ src/app/shared/notification.service.ts
@@ -14,13 +14,15 @@ export class NotificationService {
         duration: 3000,
         horizontalPosition: 'right',
         verticalPosition: 'top'
-    }
+    };
     success(msg) {
-        this.config['panelClass'] = ['notification', 'success'];
+        // this.config['panelClass']
+        this.config.panelClass = ['notification', 'success'];
         this.snackBar.open(msg, '', this.config);
     }
     warn(msg) {
-        this.config['panelClass'] = ['notification', 'warn'];
+        // this.config['panelClass']
+        this.config.panelClass = ['notification', 'warn'];
         this.snackBar.open(msg, '', this.config);
     }
 }
