17a0afe92cec45062776c5f7d7cf985a80ff0392
Randy Rebucas <rebucasrandy1986@gmail.com>
2019-08-30 08:09:54 +0800

    update upload

--- backend/controllers/upload.js
+++ backend/controllers/upload.js
@@ -2,18 +2,18 @@ const IncomingForm = require('formidable').IncomingForm
 
 exports.upload = (req, res, next) => {
   var form = new IncomingForm();
-  form.uploadDir = '../backend/attachments';
+  form.uploadDir = 'attachments';
   form.keepExtensions = true;
   form.type = 'multipart';
   form.maxFieldsSize = 20 * 1024 * 1024;
   form.maxFileSize = 200 * 1024 * 1024;
-  form.hash = false;
-  form.on('file', (field, file) => {
+  form.hash = true;
+  form.on('file', (name, file) => {
     // Do something with the file
     // e.g. save it to the database
     // you can access it using file.path
-    console.log(field);
-    console.log(file);
+    // console.log(file);
+    console.log(file.path);
   //   size: 1261541,
   // path:
   //  'C:\\Users\\Randy\\AppData\\Local\\Temp\\upload_43b4bb7ea1f94efc94ead49a5ef1e135',
@@ -30,9 +30,4 @@ exports.upload = (req, res, next) => {
   });
 
   form.parse(req);
-  // form.parse(req, function(err, fields, files) {
-  //   res.writeHead(200, {'content-type': 'text/plain'});
-  //   res.write('received upload:\n\n');
-  //   res.end(util.inspect({fields: fields, files: files}));
-  // });
 };
